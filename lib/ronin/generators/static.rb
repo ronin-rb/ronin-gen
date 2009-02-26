050413a487396d49366e43dd26e2a9a5b5b5536c
postmodern <postmodern.mod3@gmail.com>
2009-02-25 22:54:48 -0800

    Removed config.rb in favor of static.rb.
    * Use the new Static module.

--- lib/ronin/generators/static.rb
+++ lib/ronin/generators/static.rb
@@ -20,11 +20,10 @@
 #++
 #
 
+require 'ronin/static'
+
 module Ronin
   module Generators
-    module Config
-      # Static directory 
-      STATIC_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','static'))
-    end
+    Static.directory File.join(File.dirname(__FILE__),'..','..','..','static')
   end
 end
