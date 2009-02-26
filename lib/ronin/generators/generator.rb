4a806c67a365614e2919a5ae873ac3b06ee38875
postmodern <postmodern.mod3@gmail.com>
2009-02-25 20:53:47 -0800

    Added a base-class Generator.

---
+++ lib/ronin/generators/generator.rb
@@ -0,0 +1,13 @@
+module Ronin
+  module Generators
+    class Generator
+
+      #
+      # Default method which invokes the generator.
+      #
+      def generate(path)
+      end
+
+    end
+  end
+end
