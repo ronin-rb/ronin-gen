428d2d624b0b0eac07f0f6e28e3d243d5d4b267b
postmodern <postmodern.mod3@gmail.com>
2009-02-25 20:53:37 -0800

    Added Generators::Config.

---
+++ lib/ronin/generators/config.rb
@@ -0,0 +1,8 @@
+module Ronin
+  module Generators
+    module Config
+      # Static directory 
+      STATIC_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..','static'))
+    end
+  end
+end
