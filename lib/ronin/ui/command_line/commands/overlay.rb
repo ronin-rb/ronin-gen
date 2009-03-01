7bbc89ed47421649c76b72c00e66b937f28f0ab3
postmodern <postmodern.mod3@gmail.com>
2009-02-28 16:29:32 -0800

    Updated the copyright information.

--- lib/ronin/ui/command_line/commands/overlay.rb
+++ lib/ronin/ui/command_line/commands/overlay.rb
@@ -2,7 +2,7 @@
 #--
 # Ronin Gen - A Ruby library for Ronin that provides various generators.
 #
-# Copyright (c) 2009 Postmodern (postmodern.mod3 at gmail.com)
+# Copyright (c) 2009 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
