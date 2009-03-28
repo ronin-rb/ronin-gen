18e30f6a3040481b7133e1f67e4b230d36bd0896
postmodern <postmodern.mod3@gmail.com>
2009-03-27 19:36:42 -0700

    Added specs for the XML metadata file.

--- spec/generated_overlay_examples.rb
+++ spec/generated_overlay_examples.rb
@@ -1,6 +1,7 @@
 require 'ronin/platform/overlay'
 
 require 'spec_helper'
+require 'nokogiri'
 
 shared_examples_for "Generated Overlay" do
 
@@ -30,6 +31,36 @@ shared_examples_for "Generated Overlay" do
     metadata_file = File.join(@path,Ronin::Platform::Overlay::METADATA_FILE)
 
     File.file?(metadata_file).should == true
+  end
+
+  describe "XML metadata file" do
+    before(:all) do
+      @doc = Nokogiri::XML(open(File.join(@path,Ronin::Platform::Overlay::METADATA_FILE)))
+    end
+
+    it "should have the title" do
+      @doc.at('/ronin-overlay/title').inner_text.should == @title
+    end
+
+    it "should have the source URL" do
+      @doc.at('/ronin-overlay/source').inner_text.should == @source
+    end
+
+    it "should have the source-view URL" do
+      @doc.at('/ronin-overlay/source-view').inner_text.should == @source_view
+    end
+
+    it "should have the website URL" do
+      @doc.at('/ronin-overlay/website').inner_text.should == @website
+    end
+
+    it "should have the license" do
+      @doc.at('/ronin-overlay/license').inner_text.should == @license
+    end
+
+    it "should have the description" do
+      @doc.at('/ronin-overlay/description').inner_text.should == @description
+    end
   end
 
 end
