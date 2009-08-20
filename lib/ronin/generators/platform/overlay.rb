aac5e6c05097f93e58af2d47ba380b5952aa1fd5
postmodern <postmodern.mod3@gmail.com>
2009-08-20 12:46:43 -0700

    Removed the -- and ++ syntax required by RDoc.

--- lib/ronin/generators/platform/overlay.rb
+++ lib/ronin/generators/platform/overlay.rb
@@ -1,5 +1,4 @@
 #
-#--
 # Ronin Gen - A Ruby library for Ronin that provides various generators.
 #
 # Copyright (c) 2009 Hal Brodigan (postmodern.mod3 at gmail.com)
@@ -17,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#++
 #
 
 require 'ronin/generators/platform/static'
