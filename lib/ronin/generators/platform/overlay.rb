d34590cd54e12646e5c8c590e3f491d14ee80fa7
postmodern <postmodern.mod3@gmail.com>
2010-01-13 20:09:16 -0800

    Fixed another overlay.xsl path bug.

--- lib/ronin/generators/platform/overlay.rb
+++ lib/ronin/generators/platform/overlay.rb
@@ -93,7 +93,7 @@ module Ronin
           touch File.join(LIB_DIR,Ronin::Platform::Overlay::INIT_FILE)
 
           mkdir 'static'
-          copy_file File.join('ronin','static',METADATA_XSL), File.join('static',File.basename(METADATA_XSL))
+          copy_file File.join('ronin','platform',METADATA_XSL), File.join('static',METADATA_XSL)
 
           mkdir Ronin::Platform::Overlay::CACHE_DIR
           mkdir Ronin::Platform::Overlay::EXTS_DIR
