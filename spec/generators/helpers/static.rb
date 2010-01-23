ed405cc30335f4f888f58e096c417caa280ac6e5
postmodern <postmodern.mod3@gmail.com>
2010-01-23 04:59:09 -0800

    Don't reopen Ronin::Config.

--- spec/generators/helpers/static.rb
+++ spec/generators/helpers/static.rb
@@ -1,7 +1,3 @@
 require 'ronin/config'
 
-module Ronin
-  module Config
-    register_static_dir File.join(File.dirname(__FILE__),'static')
-  end
-end
+Ronin::Config.register_static_dir File.join(File.dirname(__FILE__),'static')
