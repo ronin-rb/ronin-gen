bcc6fefd15640db6399ca59ab8b6efdcb4ee4237
postmodern <postmodern.mod3@gmail.com>
2010-02-09 19:19:49 -0800

    Added specs for Gen.generator.

--- spec/gen/gen_spec.rb
+++ spec/gen/gen_spec.rb
@@ -1,3 +1,4 @@
+require 'ronin/gen/gen'
 require 'ronin/gen/version'
 
 require 'spec_helper'
@@ -5,5 +6,18 @@ require 'spec_helper'
 describe Gen do
   it "should have a version" do
     Gen.const_defined?('VERSION').should == true
+  end
+
+  it "should load generators from 'ronin/gen/generators'" do
+    generator = Gen.generator('library')
+
+    generator.should_not be_nil
+    generator.name.should == 'Ronin::Gen::Generators::Library'
+  end
+
+  it "should raise an UnknownGenerator exception on missing generators" do
+    lambda {
+      Gen.generator('lolbadfail')
+    }.should raise_error(Gen::UnknownGenerator)
   end
 end
