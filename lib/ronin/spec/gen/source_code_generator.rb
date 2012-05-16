0f4b7d5e81c5c192685453beb3e2b1f3ccf9bc03
Postmodern <postmodern.mod3@gmail.com>
2012-05-16 14:50:31 -0700

    No need to call RSpec.configure.

--- lib/ronin/spec/gen/source_code_generator.rb
+++ lib/ronin/spec/gen/source_code_generator.rb
@@ -1,9 +1,3 @@
-require 'rspec'
-
 require 'ronin/gen/source_code_generator'
 
-RSpec.configure do |spec|
-  spec.before(:suite) do
-    Ronin::Gen::SourceCodeGenerator.edit = false
-  end
-end
+Ronin::Gen::SourceCodeGenerator.edit = false
