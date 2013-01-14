636de5a1161284e582ada3abf9b94d1b178358d4
Postmodern <postmodern.mod3@gmail.com>
2013-01-13 21:50:10 -0800

    Added Ronin::Gen::Generators::Script (issue #1).

---
+++ lib/ronin/gen/generators/script.rb
@@ -0,0 +1,46 @@
+#
+# Copyright (c) 2009-2013 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of Ronin Gen.
+#
+# Ronin Gen is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ronin Gen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ronin Gen.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+require 'ronin/gen/ruby_generator'
+
+module Ronin
+  module Gen
+    module Generators
+      #
+      # A {RubyGenerator} class for creating executable Ruby scripts.
+      #
+      class Script < RubyGenerator
+
+        data_dir File.join('ronin/gen')
+
+        template 'script.rb.erb'
+
+        #
+        # Generates a new Ruby script.
+        #
+        def generate
+          super
+
+          chmod 0755, @path
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/gen/generators/script_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'ronin/gen/generators/script'
+
+require 'pathname'
+require 'tmpdir'
+
+describe Gen::Generators::Script do
+  let(:name) { 'myscript.rb' }
+  let(:path) { Pathname.new(Dir.tmpdir).join(name) }
+
+  before(:all) do
+    described_class.generate(path)
+  end
+
+  it "should create the file" do
+    path.should be_file
+  end
+
+  it "should mark the file executable" do
+    path.should be_executable
+  end
+
+  describe "the created file" do
+    subject { path.read }
+
+    it "should start with '#!/usr/bin/env ruby'" do
+      subject.lines.first.chomp.should == "#!/usr/bin/env ruby"
+    end
+
+    it "should include 'if $0 == __FILE__'" do
+      subject.should include("if $0 == __FILE__")
+    end
+  end
+
+  after(:all) do
+    path.delete
+  end
+end
